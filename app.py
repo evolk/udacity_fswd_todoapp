87faf1d42bd8022fe75bf24def460b7620fb9033
evolk <katya.volkova@gmail.com>
2020-02-09 22:32:43 +0100

    dummy app

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html', data=[
+        {'description': 'Todo 1'},
+        {'description': 'Todo 2'},
+        {'description': 'Todo 3'},
+    ])
