e0616d15aab2ff270af1b8f9bfa5868312b06a3c
evolk <katya.volkova@gmail.com>
2020-02-10 22:11:16 +0100

    added fetch method, render does not update

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 # http://127.0.0.1:5000/
 # FLASK_APP=app.py FLASK_DEBUG=true flask run
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     'postgres://evolkvolk@localhost:5432/todoapp'
-    
+
 db = SQLAlchemy(app)
 
 
@@ -17,10 +17,21 @@ class Todo(db.Model):
     description = db.Column(db.String(), nullable=False)
 
     def __repr__(self):
-        return f'<Todo {self.id} {self.description}'
+        return f'<Todo {self.id} {self.description}>'
 
 
 db.create_all()
+
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    description = request.get_json()['description']
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    return jsonify({
+        'description': todo.description
+    })
 
 
 @app.route('/')
